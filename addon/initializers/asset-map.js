c341ee680a5de2e2a687398cf3a4e2c66286cb0f
Florian Fischer <florian.fischer@viasto.com>
2017-04-25 17:07:12 +0200

    adapt addon for Ember cli 1.13

--- addon/initializers/asset-map.js
+++ addon/initializers/asset-map.js
@@ -1,46 +1,20 @@
-/* global window, __assetMapPlaceholder__ */
-import RSVP from 'rsvp';
-import $ from 'jquery';
+/* global window, __assetMapFilename__ */
+import Ember from 'ember';
 import AssetMap from '../services/asset-map';
 
 export function initialize(app) {
-  let config = app.__container__.lookupFactory('config:environment');
+  let assetMapFile = window && window.__assetMapFilename__;
 
-  let assetMapFile = window && window.__assetMapPlaceholder__;
+  const promise = new Ember.RSVP.Promise((resolve, reject) => {
+    Ember.$.getJSON(assetMapFile, resolve).fail(reject);
+  });
 
-  if (!assetMapFile) {
-    app.register('service:asset-map', AssetMap);
-    return;
-  }
-
-  if (config.ifa.inline) {
-    AssetMap.reopen({
-      map: assetMapFile.assets,
-      prepend: assetMapFile.prepend,
-      enabled: true
-    });
-    app.register('service:asset-map', AssetMap);
-  } else {
-    app.deferReadiness();
-
-    const promise = new RSVP.Promise((resolve, reject) => {
-      $.getJSON(assetMapFile, resolve).fail(reject);
-    });
-
-    promise.then((map = {}) => {
-      AssetMap.reopen({
-        map: map.assets,
-        prepend: map.prepend,
-        enabled: true
-      });
-    }).then(() => {
-      app.register('service:asset-map', AssetMap);
-      app.advanceReadiness();
-    });
-  }
+  promise.then((map = {}) => {
+    window.assetMap = map.assets;
+  });
 }
 
 export default {
   name: 'asset-map',
-  initialize
+  initialize: initialize
 };
